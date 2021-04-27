f07b07a88ec50e7d865fb5ffa9a5c1adc1321f33
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-04-27 22:18:35 +0200

    Working on

--- 01_01/begin/EvenOrOdd/src/main/java/EvenOrOdd.java
+++ 01_01/begin/EvenOrOdd/src/main/java/EvenOrOdd.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class EvenOrOdd {
 
    public static boolean isEven(int n) {
-       return true;
+      return n%2==0;
+      
    }
 
    public static void main(String[] args) {
